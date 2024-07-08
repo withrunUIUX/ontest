0e18a4476bc3ddb1618231186be8be0f389ca653
jseonae <128363969+jseonae@users.noreply.github.com>
2024-07-08 18:35:18 +0900

    Update contents.js

--- js/contents.js
+++ js/contents.js
@@ -18,51 +18,20 @@ function popupTop(){
 }
 
 $(document).ready(function(){
-    $('.designSelect,.designSelect.type01, .designSelect.type02, .designSelect.type03, .designSelect.type04').on('click', function (event) {
-        $(this).toggleClass('on');
-    });
-    
-    $('body').on('click', function (e) {
-        if (!$('.designSelect,.designSelect.type01, .designSelect.type02, .designSelect.type03, .designSelect.type04').is(e.target)
-        && $('.designSelect,.designSelect.type01, .designSelect.type02, .designSelect.type03, .designSelect.type04').has(e.target).length === 0 
-        && $('.on').has(e.target).length === 0
-        ) {
-            $('.designSelect.designSelect.type01, .designSelect.type02, .designSelect.type03, .designSelect.type04').removeClass('on');
-        }
-    });
-    
-    $('#stageBtn01, #stageBtn02, #stageBtn03').on('click', function (event) {
-        $(this).toggleClass('on');
-        $('.stage__wrap').toggleClass('on');
-        $('.grap__wrap').toggleClass('off');
-        if($('.stage__wrap').hasClass('on')){
-            moveScroll();
-        }
-    });
-    
-    // checkbox 전체선택
-    $("#step1_chk_all").click(function(){
-        if($(this).prop("checked")){
-            $(".step1_chk").prop("checked", true);
-        } else {
-            $(".step1_chk").prop("checked", false);
-        }
-    });
-    $("#step2_chk_all").click(function(){
-        if($(this).prop("checked")){
-            $(".step2_chk").prop("checked", true);
-        } else {
-            $(".step2_chk").prop("checked", false);
-        }
-    });
-    $('#step3_chk_all').click(function(){
-        if($(this).prop("checked")){
-            $(".step3_chk").prop("checked", true);
-        } else {
-            $(".step3_chk").prop("checked", false);
-        }
-    });
-
+	//체크박스 전체 선택, 해제
+	$("#allcheck").click(function() {
+		if($("#allcheck").is(":checked"))
+		$("input[name=check]").prop("checked", true);
+		else $("input[name=check]").prop("checked", false);
+	});
+	
+	$("input[name=check]").click(function() {
+		var total = $("input[name=check]").length;
+		var checked = $("input[name=check]:checked").length;
+		
+		if(total != checked) $("#allcheck").prop("checked", false);
+		else $("#allcheck").prop("checked", true); 
+	});
  
 	// 탭 컨텐츠 숨기기
     $(".tab-body").hide();
