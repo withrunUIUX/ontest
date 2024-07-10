f7bd2f7d34748d45cd39e60ba38033ffc2aa3260
jseonae <128363969+jseonae@users.noreply.github.com>
2024-07-10 13:27:45 +0900

    Update include.js

--- js/include.js
+++ js/include.js
@@ -1,4 +1,8 @@
 $(function(){
+    //일반페이지
     $("#header").load("../include/header.html"); 
     $("#footer").load("../include/footer.html"); 
+
+    //문항페이지
+    $("#question_header").load("../include/question_header.html"); 
 });
