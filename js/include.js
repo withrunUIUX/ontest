69c0239041adad354c2b2dc298eb03371371caf2
jseonae <128363969+jseonae@users.noreply.github.com>
2024-08-19 10:49:39 +0900

    복제

--- js/include.js
+++ js/include.js
@@ -1,9 +1,9 @@
 $(function(){
     //일반페이지
-    $("#header").load("../include/header.html"); 
-    $("#footer").load("../include/footer.html"); 
+    $("#header").load("/include/header.html"); 
+    $("#footer").load("/include/footer.html"); 
 
     //문항페이지
-    $("#question_header").load("../include/question_header.html"); 
-    $("#question_footer").load("../include/question_footer.html"); 
+    $("#question_header").load("/include/question_header.html"); 
+    $("#question_footer").load("/include/question_footer.html"); 
 });
